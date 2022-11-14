b376883ab8331b0435d9607b8e41d0de03f0c459
Owusu-Desmond <desmond0406114009@gmail.com>
2022-11-14 14:29:26 +0000

    Add login and signout views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: ''
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users
 end
