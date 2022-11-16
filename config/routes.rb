27b74203ee13aa11a8bf2d28ea4cf6885f49736e
Owusu-Desmond <desmond0406114009@gmail.com>
2022-11-16 08:25:21 +0000

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users
-  resources :foods
+  root "recipes#index"
+
+  resources :users do 
+    resources :foods
+    resources :recipes
+  end
 end
