2da2fb30a8ffe711400dae3a66d639e0372ef934
Collins <mussocollins@gmail.com>
2022-11-16 17:38:14 +0300

    Add inventories

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,13 +1,11 @@
-# frozen_string_literal: true
-
 class InventoriesController < ApplicationController
   def index
     @inventories = Inventory.all
   end
 
-  # def show
-  #   @inventory = Inventory.find(params[:id])
-  # end
+  def show
+    @inventory = Inventory.find(params[:id])
+  end
 
   def new
     @inventory = Inventory.new
@@ -15,19 +13,20 @@ class InventoriesController < ApplicationController
 
   def create
     @inventory = Inventory.new(inventory_params)
+    @inventory.user = current_user
 
     if @inventory.save
       flash[:success] = 'Inventory added successfully'
-      redirect_to inventories_path
+      redirect_to user_inventories_path
     else
       flash.now[:error] = 'Error: Inventory could not be added'
-      redirect_to new_inventory_path
+      redirect_to new_user_inventory_path
     end
   end
 
   def destroy
     Inventory.find(params[:id]).delete
-    redirect_to inventories_path
+    redirect_to user_inventories_path
   end
 
   private
