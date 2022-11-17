f6ac20716d9006d4d224c275f03d0b7765f82526
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-11-17 10:17:30 +0100

    Add methods for counting total food quantity and price for a recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,17 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
+
+  def count_total_price
+    total = 0
+    self.recipe_foods.each do |rf|
+      food = Food.find(rf.food_id)
+      total += food.price
+    end
+    total
+  end
+
+  def count_total_food_items
+    self.recipe_foods.sum('quantity')
+  end
 end
