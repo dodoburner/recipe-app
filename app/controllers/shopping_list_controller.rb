6c548281c30a9f6f0e8d14f6fe1a3a2a6dde778b
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-11-18 19:46:21 +0100

    Create functionality for shopping list

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -6,17 +6,30 @@ class ShoppingListController < ApplicationController
     @inventory_foods = @inventory.inventory_foods.includes(:food)
 
     @missing_foods = []
+    @total_value = 0
 
-    # @recipe_foods.each do |r_food|
-    #   @inventory_foods.each do |i_food|
-    #     if r_food.food.name == i_food.food.name
-    #       @missing_food = r_food.food
-    #       quantity = r_food.quantity - i_food.quantity
-    #       @missing_food['quantity'] = quantity
-    #       @missing_foods << quantity unless quantity <= 0
-    #     end
-    #   end
-    # end
+    @recipe_foods.each do |r_food|
+      @has_food = false
+      @inventory_foods.each do |i_food|
+        if r_food.food.name == i_food.food.name
+          @has_food = true
+          quantity = r_food.quantity - i_food.quantity
+          price = r_food.food.price.to_f / r_food.quantity
+          price_for_missing_quantity = (quantity * price).round
+          @missing_food = {name: r_food.food.name, quantity: quantity, price: price_for_missing_quantity, measurement_unit: r_food.food.measurement_unit}
+          if (quantity > 0)
+            @missing_foods << @missing_food unless quantity <= 0
+            @total_value += price_for_missing_quantity
+          end
+        end
+      end
+      if (!@has_food)
+        food = r_food.food
+        @missing_food = {name: food.name, quantity: r_food.quantity, price: food.price, measurement_unit: food.measurement_unit}
+        @missing_foods << @missing_food
+        @total_value += food.price
+      end
+    end
   end
 
   def new
