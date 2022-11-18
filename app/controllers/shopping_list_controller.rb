40813deca763449202b3782d534e7baf332ccc7f
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-11-18 16:50:35 +0100

    Attempt to display missing foods

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -2,6 +2,21 @@ class ShoppingListController < ApplicationController
   def index
     @inventory = Inventory.find(params[:inventories])
     @recipe = Recipe.find(params[:recipe])
+    @recipe_foods = @recipe.recipe_foods.includes(:food)
+    @inventory_foods = @inventory.inventory_foods.includes(:food)
+
+    @missing_foods = []
+
+    # @recipe_foods.each do |r_food|
+    #   @inventory_foods.each do |i_food|
+    #     if r_food.food.name == i_food.food.name
+    #       @missing_food = r_food.food
+    #       quantity = r_food.quantity - i_food.quantity
+    #       @missing_food['quantity'] = quantity
+    #       @missing_foods << quantity unless quantity <= 0
+    #     end
+    #   end
+    # end
   end
 
   def new
