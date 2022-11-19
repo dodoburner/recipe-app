72d30bfa8db79f2348681e51e95bf0101c66688a
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-11-19 09:50:39 +0100

    Update users routing and public recipes show

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get 'inventory_foods/index'
-  # get 'inventory_foods/new'
-  # get 'inventory_foods/show'
-  # get 'inventories/index'
-  # get 'inventories/show'
-  # get 'inventories/new'
-  # devise_for :users, path: ''
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  devise_for :users, sign_out_via: [:get, :post]
+  devise_for :users
 
   resources :recipes, only: [:index, :new, :show, :destroy, :create] do
     resources :recipe_foods
